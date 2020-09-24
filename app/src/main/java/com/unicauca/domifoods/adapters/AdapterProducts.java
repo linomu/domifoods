2fa14f9c269059b9b02a6dc5a7d129c2d05c485a
lino <linomu@unicauca.edu.co>
2020-09-23 23:09:42 -0500

    La vista de ProductsFragment ya tiene asociado el AdapterCategories y AdapterProducts Hace falta lanzar el ProductsFragment con base en el restaruante que el cliente ha elegido.

---
+++ app/src/main/java/com/unicauca/domifoods/adapters/AdapterProducts.java
@@ -0,0 +1,66 @@
+package com.unicauca.domifoods.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+import com.unicauca.domifoods.R;
+import com.unicauca.domifoods.domain.Product;
+import com.unicauca.domifoods.settings.CircleTransform;
+
+import java.util.ArrayList;
+
+public class AdapterProducts extends RecyclerView.Adapter<AdapterProducts.holderProducts> {
+
+
+    /*Variables that we're gonna use*/
+    ArrayList<Product> products;
+    Picasso mPicasso;
+
+    public AdapterProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    @NonNull
+    @Override
+    public holderProducts onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_products, parent, false);
+        mPicasso = new Picasso.Builder(view.getContext())
+                .indicatorsEnabled(false)
+                .build();
+        return new holderProducts(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull holderProducts holder, int position) {
+        holder.name_product.setText(products.get(position).getName());
+        holder.price.setText("$ "+products.get(position).getPrice().toString());
+        mPicasso.load(products.get(position).getImage())
+                .transform(new CircleTransform())
+                .into(holder.img_product);
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public class holderProducts extends RecyclerView.ViewHolder {
+        ImageView img_product;
+        TextView name_product, price;
+        public holderProducts(@NonNull View itemView) {
+            super(itemView);
+            img_product = itemView.findViewById(R.id.img_product);
+            name_product = itemView.findViewById(R.id.tv_name_product);
+            price = itemView.findViewById(R.id.tv_price_product);
+        }
+    }
+}
--- app/src/main/java/com/unicauca/domifoods/fragments/ProductsFragment.java
+++ app/src/main/java/com/unicauca/domifoods/fragments/ProductsFragment.java
@@ -17,8 +17,10 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 import com.unicauca.domifoods.R;
 import com.unicauca.domifoods.adapters.AdapterCategories;
+import com.unicauca.domifoods.adapters.AdapterProducts;
 import com.unicauca.domifoods.adapters.AdapterRestaurants;
 import com.unicauca.domifoods.domain.Category;
+import com.unicauca.domifoods.domain.Product;
 import com.unicauca.domifoods.domain.Restaurant;
 import com.unicauca.domifoods.settings.CircleTransform;
 
@@ -30,8 +32,9 @@ public class ProductsFragment extends Fragment {
 
     ImageView img_restaurant_icon, img_restaurant_product_bg;
     Picasso mPicasso;
-    RecyclerView recyclerView;
+    RecyclerView recyclerView, recyclerView_products;
     ArrayList<Category> categories;
+    ArrayList<Product> products;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -98,6 +101,7 @@ public class ProductsFragment extends Fragment {
         Picasso.with(getContext()).load("https://cdn.jpegmini.com/user/images/slider_puffin_jpegmini_mobile.jpg").transform(new CircleTransform()).into(img_restaurant_icon);
 
         setUpTheRecyclerView(view);
+        setUpTheRecyclerViewProducts(view);
 
     }
 
@@ -111,12 +115,29 @@ public class ProductsFragment extends Fragment {
         recyclerView.setAdapter(adapterCategories);
     }
 
+
+    private void setUpTheRecyclerViewProducts(View view) {
+        products = new ArrayList<>();
+        recyclerView_products = view.findViewById(R.id.recycler_products);
+        recyclerView_products.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        fillOutTheProducts();
+        AdapterProducts adapterProducts = new AdapterProducts(products);
+        recyclerView_products.setAdapter(adapterProducts);
+    }
+
     public void  fillOutTheCategories(){
         categories.add(new Category("Almuerzos","https://i.pinimg.com/originals/d1/e9/8c/d1e98c995db0af92a858e7ccf023b37d.jpg"));
         categories.add(new Category("Jugos","https://cdn.kiwilimon.com/recetaimagen/30632/34260.jpg"));
         categories.add(new Category("Postres","https://okdiario.com/img/2018/06/09/tarta-de-limon-655x368.jpg"));
         categories.add(new Category("Café","https://www.juanvaldezcafe.com/sites/default/files/tinto_grande.jpg"));
         categories.add(new Category("Helados","https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2020/06/10/helado-de-stracciatella.jpeg"));
+    }
+    public void  fillOutTheProducts(){
+        products.add(new Product("Arroz con Pollo","https://s1.eestatic.com/2020/01/28/cocinillas/recetas/pasta-y-arroz/Arroz-Pollo-Pasta_y_arroz_463216136_143694761_1024x576.jpg", (float) 15000));
+        products.add(new Product("Caldo de Res","https://www.deliciosi.com/images/100/150/caldo-de-res.jpg", (float)12000));
+        products.add(new Product("Sancocho","https://t1.rg.ltmcdn.com/es/images/1/8/1/img_sancocho_de_gallina_o_pollo_12181_orig.jpg", (float)10000));
+        products.add(new Product("Ajiaco","https://images-gmi-pmc.edge-generalmills.com/4a994b44-4d9c-4552-82e4-6e9964322a78.jpg",(float)8500));
+        products.add(new Product("Frijoles","https://t2.uc.ltmcdn.com/images/8/1/4/img_como_hacer_frijoles_colombianos_31418_orig.jpg", (float)7500));
     }
 }
 
