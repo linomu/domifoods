9c9c7cec97e607fee949e0af4bfb4480071e3ad4
lino <linomu@unicauca.edu.co>
2020-10-02 00:23:29 -0500

    Ya capturé los datos desde la interfaz de registro1 y los llevo a la segunda interfaz, me hace faltar terminar de solicitar toda la información y tenerla en la clase java Register2Activity.java.

--- app/src/main/java/com/unicauca/domifoods/Register1Activity.java
+++ app/src/main/java/com/unicauca/domifoods/Register1Activity.java
@@ -6,19 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class Register1Activity extends AppCompatActivity implements View.OnClickListener {
 
-    Button buttonSiguiente;
+    public static final String USER_NAME = "user_name";
+    public static final String USER_LASTNAME = "user_last_name";
+    public static final String USER_KIND_ID = "user_kind_id";
+    public static final String USER_ID = "user_id";
+    public static final String USER_PHONE = "user_phone";
+
+    private Button btn_next_register;
+    private EditText et_user_name, et_user_lastname, et_id_user, et_phone_user;
+    private Spinner sp_kind_of_id_user;
+    private String user_name, user_last_name, user_id, phone, kind_of_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
-        buttonSiguiente = findViewById(R.id.buttonSiguiente);
-        buttonSiguiente.setOnClickListener(this);
+        /*In the next method, I make the initialization of variables*/
+        initializationVariables();
+    }
 
+    private void initializationVariables() {
+        et_user_name = findViewById(R.id.et_user_name);
+        et_user_lastname = findViewById(R.id.et_user_lastname);
+        et_id_user = findViewById(R.id.et_user_id);
+        et_phone_user = findViewById(R.id.et_user_phone);
+        sp_kind_of_id_user = findViewById(R.id.sp_kind_of_id);
+        btn_next_register = findViewById(R.id.btn_next_register);
+        btn_next_register.setOnClickListener(this);
     }
 
     @Override
@@ -34,12 +54,55 @@ public class Register1Activity extends AppCompatActivity implements View.OnClick
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.buttonSiguiente:
-                Intent intent1 = new Intent(Register1Activity.this, Register2Activity.class);
-                startActivity(intent1);
+        switch (view.getId()) {
+            case R.id.btn_next_register:
+                //In the next method, I'll validate that the form is filled out
+                if (isTheFormComplete()==false) {
+                    Intent intent = new Intent(Register1Activity.this, Register2Activity.class);
+                    //Create the bundle
+                    Bundle bundle = new Bundle();
+                    bundle.putString(USER_NAME, user_name);
+                    bundle.putString(USER_LASTNAME, user_last_name);
+                    bundle.putString(USER_KIND_ID, kind_of_id);
+                    bundle.putString(USER_ID, user_id);
+                    bundle.putString(USER_PHONE, phone);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
                 break;
 
         }
+    }
+
+    private boolean isTheFormComplete() {
+        boolean isTheFormComplete = true;
+        user_name = et_user_name.getText().toString().trim();
+        user_last_name = et_user_lastname.getText().toString().trim();
+        user_id = et_id_user.getText().toString().trim();
+        phone = et_phone_user.getText().toString().trim();
+        kind_of_id = sp_kind_of_id_user.getSelectedItem().toString().trim();
+
+        if(user_name.isEmpty()){
+            et_user_name.setError(getResources().getString(R.string.msg_required_filed));
+            et_user_name.requestFocus();
+            isTheFormComplete = false;
+        }
+        if(user_last_name.isEmpty()){
+            et_user_lastname.setError(getResources().getString(R.string.msg_required_filed));
+            et_user_lastname.requestFocus();
+            isTheFormComplete = false;
+        }
+        if(user_id.isEmpty()){
+            et_id_user.setError(getResources().getString(R.string.msg_required_filed));
+            et_id_user.requestFocus();
+            isTheFormComplete = false;
+        }
+        if(phone.isEmpty()){
+            et_phone_user.setError(getResources().getString(R.string.msg_required_filed));
+            et_phone_user.requestFocus();
+            isTheFormComplete = false;
+        }
+
+     return isTheFormComplete;
     }
 }
--- app/src/main/java/com/unicauca/domifoods/Register2Activity.java
+++ app/src/main/java/com/unicauca/domifoods/Register2Activity.java
@@ -8,24 +8,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class Register2Activity extends AppCompatActivity {
+public class Register2Activity extends AppCompatActivity implements View.OnClickListener {
 
-    Button etPlannedDate;
+
+    private String user_name, user_last_name, user_id, phone, kind_of_id;
+    private String user_birthday, user_email, user_address, user_gender, user_password;
+    
+    private Button btn_birth_day, btn_register;
+    private EditText et_user_email, et_user_address;
+    private RadioGroup radioGroup_gender;
+    private RadioButton et_user_gender;
+
     Calendar C = Calendar.getInstance();
     //private int nYarIni, nMonthIni, nDayIni, sYearIni, sMonthIni, sDayUni;
 
 
-    //= (EditText) view.findViewById(R.id.etPlannedDate);
-    //etPlannedDate.setOnClickListener(this);
+    //= (EditText) view.findViewById(R.id.btn_birth_day);
+    //btn_birth_day.setOnClickListener(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        /*In the next method, I make the initialization of variables*/
+        initializationVariables();
+        //Retrieve the information through out the bundle object
+        retrieveInformation();
 /*
         new DatePickerDialog(Register2Activity.this, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -35,14 +49,26 @@ public class Register2Activity extends AppCompatActivity {
         }, 2015, 02, 26).show();
 
 */
-        etPlannedDate = findViewById(R.id.etPlannedDate);
-        etPlannedDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new DatePickerDialog(Register2Activity.this, R.style.DialogTheme, date, C.get(Calendar.YEAR), C.get(Calendar.MONTH), C.get(Calendar.DAY_OF_MONTH)).show();
 
-            }
-        });
+    }
+
+    private void initializationVariables() {
+        et_user_email = findViewById(R.id.et_user_email);
+        et_user_address = findViewById(R.id.et_user_address);
+        radioGroup_gender = findViewById(R.id.radio_gender);
+        btn_birth_day = findViewById(R.id.btn_user_birth_day);
+        btn_birth_day.setOnClickListener(this);
+        btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(this);
+    }
+
+    private void retrieveInformation() {
+        Bundle bundle = getIntent().getExtras();
+        user_name = bundle.getString(Register1Activity.USER_NAME);
+        user_last_name = bundle.getString(Register1Activity.USER_LASTNAME);
+        user_id = bundle.getString(Register1Activity.USER_ID);
+        phone = bundle.getString(Register1Activity.USER_PHONE);
+        kind_of_id = bundle.getString(Register1Activity.USER_KIND_ID);
     }
 
     DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
@@ -63,7 +89,7 @@ public class Register2Activity extends AppCompatActivity {
         String formatoDeFecha = "MM/dd/yy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(formatoDeFecha, Locale.US);
 
-        etPlannedDate.setText(sdf.format(C.getTime()));
+        btn_birth_day.setText(sdf.format(C.getTime()));
     }
 
     @Override
@@ -78,4 +104,25 @@ public class Register2Activity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case  R.id.btn_next_register:
+                if (isTheFormComplete()==false) {
+
+                }
+                break;
+            case R.id.btn_user_birth_day:
+                new DatePickerDialog(Register2Activity.this, R.style.DialogTheme, date, C.get(Calendar.YEAR), C.get(Calendar.MONTH), C.get(Calendar.DAY_OF_MONTH)).show();
+                break;
+        }
+    }
+
+    private boolean isTheFormComplete() {
+        boolean isTheFormComplete = true;
+        user_email = et_user_email.getText().toString().trim();
+        user_birthday = btn_birth_day.getText().toString().trim();
+        
+        return isTheFormComplete;
+    }
 }
