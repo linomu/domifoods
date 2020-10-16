b9857a1eb80687dd58e04e8ae7333a0218427e64
javiersee <edwarcuasapud@gmail.com>
2020-10-16 00:03:39 -0500

    docuementacion del gson de lino  para  eel api user   para consumir  el servicio web mediante direccionamiento url

--- app/src/main/java/com/unicauca/domifoods/apiUser/ApiUser.java
+++ app/src/main/java/com/unicauca/domifoods/apiUser/ApiUser.java
@@ -63,11 +63,10 @@ public interface ApiUser {
     // method for the list of categories for restaurnts this send  id and id resaurants
     @GET("/restaurants/api/restaurants/{id}/category/{idCat}/products/")
     Call<List<ProductResponse>>getProductsByCategoryAndRestaurant(@Path("id") int idRestaurant, @Path("idCat") int idCategoria);
-
+    // method for the list  of restaurnts this send  id and id resaurants
     @GET("restaurants/api/restaurants/{id}/")
     Call<RestaurantResponse>getInfoRestaurantByID(@Path("id") int idRestaurant);
-
-
+    // method for the   of restaurnts
     @GET("restaurants")
     Call<GetRestaurant> list();
 }
