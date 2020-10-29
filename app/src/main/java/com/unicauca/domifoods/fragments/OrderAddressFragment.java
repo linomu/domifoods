aa8bcccfd2cc723095ed635b5a5eef2c21f04c09
lino <linomu@unicauca.edu.co>
2020-10-29 16:31:44 -0500

    Funciona el mapa

--- app/src/main/java/com/unicauca/domifoods/fragments/OrderAddressFragment.java
+++ app/src/main/java/com/unicauca/domifoods/fragments/OrderAddressFragment.java
@@ -2,6 +2,8 @@ package com.unicauca.domifoods;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -60,5 +62,14 @@ public class OrderAddressFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_order_address, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MapsFragment fr = new MapsFragment();
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container,fr)
+                .commit();
     }
 }
